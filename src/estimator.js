e0d6e2d45dbe25a51b7f4a19bee07d87c1e09e41
themyd001 <geniustechng@gmail.com>
2020-04-09 11:00:26 +0100

    Finshed Assessment

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,63 @@
-const covid19ImpactEstimator = (data) => data;
+function getIBRTS(x, y)
+{
+    let remainder, quotient;
+    if(x == 'weeks')
+    {
+        y = y * 7;
+    }
+    else if(x == 'months')
+    {
+        y = y * 30;
+    }
 
+    remainder = y%3;
+    y = y - remainder;
+    quotient = y/3;
+    return quotient;
+}
+
+const covid19ImpactEstimator = (data) => {
+        const allData = data;
+        const getIBRT = getIBRTS(allData.periodType, allData.timeToElapse);
+        const getIBR = 2 ** getIBRT;
+        let impact, severeImpact = {};
+
+        //Challenge 1
+
+        //a
+        impact.currentlyInfected = allData.reportedCases * 10;
+        severeImpact.currentlyInfected = allData.reportedCases * 50;
+
+        //b
+        impact.infectionsByRequestedTime = impact.currentlyInfected * getIBR;
+        severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * getIBR;
+
+        //Challenge 2
+
+        //a
+        impact.severeCasesByRequestedTime = impact.infectionsByRequestedTime * 0.15;
+        severeImpact.severeCasesByRequestedTime = severeImpact.infectionsByRequestedTime * 0.15;
+
+        //b
+        impact.hospitalBedsByRequestedTime = (allData.totalHospitalBeds * 0.35) - impact.severeCasesByRequestedTime;
+        severeImpact.hospitalBedsByRequestedTime = (allData.totalHospitalBeds * 0.35) - severeImpact.severeCasesByRequestedTime;
+
+        //Challenge 3
+
+        //a
+        impact.casesForICUByRequestedTime = impact.infectionsByRequestedTime * 0.05;
+        severeImpact.casesForICUByRequestedTime = severeImpact.infectionsByRequestedTime * 0.05;
+
+        //b
+        impact.casesForVentilatorsByRequestedTime = impact.infectionsByRequestedTime * 0.02;
+        severeImpact.casesForVentilatorsByRequestedTime = severeImpact.infectionsByRequestedTime * 0.02;
+
+        //c
+        impact.dollarsInFlight = impact.infectionsByRequestedTime * allData.region.avgDailyIncomePopulation * allData.region.avgDailyIncomeInUSD * getIBRT;
+        severeImpact.dollarsInFlight = severeImpact.infectionsByRequestedTime * allData.region.avgDailyIncomePopulation * allData.region.avgDailyIncomeInUSD * getIBRT;
+
+        return{data, impact, severeImpact};
+
+};
+ 
 export default covid19ImpactEstimator;
